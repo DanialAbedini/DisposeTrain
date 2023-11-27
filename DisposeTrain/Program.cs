4dda355928069a4747555a0a884fbf316fa981de
GreatLuci <danialabedini1380@gmail.com>
2023-11-27 07:51:11 -0800

    Add project files.

---
+++ DisposeTrain/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+
+
+public class FileReader : IDisposable
+{
+    private StreamReader reader;
+
+    public FileReader(string filePath)
+    {
+        reader = new StreamReader(filePath);
+    }
+
+
+    public string ReadFileContent()
+    {
+        return reader.ReadToEnd();
+    }
+    public void Dispose()
+    {
+        if (reader != null)
+        {
+            reader.Dispose();
+            reader = null;
+        }
+    }
+}
+
+public class ByUsing
+{
+    public void ReadAndPrintFileContent(string filePath)
+    {
+        using (FileReader fileReader = new FileReader(filePath))
+        {
+            string content = fileReader.ReadFileContent();
+            Console.WriteLine("File content: " + content);
+        }
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        ByUsing byusing = new ByUsing();
+        byusing.ReadAndPrintFileContent("C:\\Users\\Lucifer\\Desktop\\exa.txt");
+    }
+}
